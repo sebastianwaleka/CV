b6a72b56ab033a30e33cd2ade60071b61e18fee4
sebastianwaleka <sebastian.waleka@gmail.com>
2018-10-02 21:17:33 +0200

    Added function add_employee and print_employee. Added test for add_employee

--- json/employee.py
+++ json/employee.py
@@ -1,11 +1,39 @@
 '''
 This program will add and display employees written in json file.
 '''
+import json
+
 
 class Employee:
-    def __init__(self, name, lastname, salary, age):
+    def __init__(self, name, lastname, salary, birth_year):
         self.name = name
         self.lastname = lastname
         self.salary = salary
-        self.age = age
+        self.birth_year = birth_year
 
+    def add_employee(self):
+        try:
+            with open('employee.json') as f:
+                self.employees = json.load(f)
+        except FileNotFoundError:
+            self.employees = []
+
+        employee = {
+            'name': self.name,
+            'last_name': self.lastname,
+            'salary': self.salary,
+            'birth_year': self.birth_year
+        }
+
+        self.employees.append(employee)
+        with open('employee.json', 'w') as f:
+            json.dump(self.employees, f)
+
+        return self.employees
+
+    def print_employees(self):
+        with open('employee.json') as f:
+            data = json.load(f)
+        for num, emp in enumerate(data):
+            print(f'[{num}] - {emp["name"]} {emp["lastname"]} '
+                  f'- SALARY: {emp["salary"]} - BIRTH YEAR: {emp["birth_year"]};')
--- json/employees_test.py
+++ json/employees_test.py
@@ -1,12 +1,20 @@
 '''
 Tests for employees.
 '''
-from employee import *
-
+import employee
 
 def test_create_Employee():
-    employee = Employee('Jan', 'Kowalski', 5000, 40)
-    assert employee.name == 'Jan'
-    assert employee.lastname == 'Kowalski'
-    assert employee.salary == 5000
-    assert employee.age == 40
+    emp = employee.Employee('Jan', 'Kowalski', 5000, 1970)
+    assert emp.name == 'Jan'
+    assert emp.lastname == 'Kowalski'
+    assert emp.salary == 5000
+    assert emp.birth_year == 1970
+
+def test_add_new_employee():
+    emp = employee.Employee('Jan', 'Kowalski', 5000, 1970)
+    assert emp.add_employee() == [{
+        'name': 'Jan',
+        'last_name': 'Kowalski',
+        'salary': 5000,
+        'birth_year': 1970
+    }]
