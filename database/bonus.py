18f174f3662069d9e3be40f95a2137e5858edf4d
sebastianwaleka <sebastian.waleka@gmail.com>
2018-11-25 19:51:57 +0100

    Deleted class Branch, moved func add_to_database to class DataBase

--- database/bonus.py
+++ database/bonus.py
@@ -1,9 +1,10 @@
 import sqlite3
 
+
 class DataBase():
-    def __init__(self, db_name, *args):
-        self.db_name = db_name
-        self.args = args
+    def __init__(self, db_name, *db_args):
+        self.db_name = db_name  # Name of Data Base
+        self.db_args = db_args  # Columns of just named Data Base
         try:
             conn = sqlite3.connect(f'{db_name}.sqlite')
         except:
@@ -13,29 +14,20 @@ class DataBase():
 
         cur = conn.cursor()
         cur.execute(f'DROP TABLE IF EXISTS {db_name}')
-        cur.execute(f'CREATE TABLE {db_name} {args}')
+        cur.execute(f'CREATE TABLE {db_name} {db_args}')
         conn.commit()
         conn.close()
 
-class Branch():
-    def __init__(self, branch_name, target, sale = 0):
-        self.branch_name = branch_name
-        self.target = target
-        self.sale = sale
-
-    def add_to_database(self):
-        conn = sqlite3.connect('branches.sqlite')
+    def add_to_database(self, *args):
+        conn = sqlite3.connect(f'{self.db_name}.sqlite')
         cur = conn.cursor()
-        cur.execute('DROP TABLE IF EXISTS branches')
-        cur.execute('CREATE TABLE branches (num INT, branch TEXT, target FLOAT, sale FLOAT)')
-        cur.execute('INSERT INTO branches (num, branch, target, sale) VALUES (?, ?, ?, ?)',
-                    ('NULL', self.branch_name, self.target, self.sale))
+        cur.execute(f'INSERT INTO {self.db_name} {self.db_args} VALUES {args}')
         conn.commit()
         conn.close()
 
     # def select_branch(self):
 
-db = DataBase('employees', 'name','last_name')
 
-war = Branch('Warszawa',2850000)
-war.add_to_database()
+db = DataBase('employees', 'name', 'last_name')
+
+db.add_to_database('Sebastian', 'Waleka')
