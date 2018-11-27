c474030ab9792572acf39870827919c02208dd08
sebastianwaleka <sebastian.waleka@gmail.com>
2018-11-27 16:23:13 +0100

    Fixed adding new rows to table

--- database/bonus.py
+++ database/bonus.py
@@ -8,45 +8,59 @@ class DataBase():
             with open(f'{db_name}.sqlite') as db:
                 db.read()
             print(f'Data base named {db_name} already exists.')
-            # conn = sqlite3.connect(f'{db_name}.sqlite')
         except:
             with open(f'{db_name}.sqlite', 'w') as db:
                 db.read()
             print(f'Created data base named {db_name}.')
-            # conn = sqlite3.connect(f'{db_name}.sqlite')
 
 
 class Table():
-    table_columns = ()
-    def __init__(self, db_name, table_name):
+
+    def __init__(self, db_name, table_name, *tab_col):
         self.db_name = db_name
         self.table_name = table_name
+        self.tab_col = tab_col
 
-    def create_table(self, *args):
-        self.table_columns = self.table_columns + args
+    def create_table(self):
         conn = sqlite3.connect(f'{self.db_name}.sqlite')
         cur = conn.cursor()
         cur.execute(f'DROP TABLE IF EXISTS {self.table_name}')
-        cur.execute(f'CREATE TABLE {self.table_name} {args}')
+        cur.execute(f'CREATE TABLE {self.table_name} {self.tab_col}')
         conn.commit()
         conn.close()
 
     def add_to_table(self, *tab_args):
-        if len(tab_args) == len(self.table_columns):
-            conn = sqlite3.connect(f'{self.db_name}.sqlite')
-            cur = conn.cursor()
-            cur.execute(f'INSERT INTO {self.table_name} {self.table_columns} VALUES {tab_args}')
-            conn.commit()
-            conn.close()
-        else:
-            print('Number of arguments must be equal to number of columns in table')
-            print(f'{len(tab_args)} - {len(self.table_columns)}')
+        conn = sqlite3.connect(f'{self.db_name}.sqlite')
+        cur = conn.cursor()
+        cur.execute(f'INSERT INTO {self.table_name} {self.tab_col[1:]} VALUES {tab_args}')
+        conn.commit()
+        conn.close()
 
-    # def select_branch(self):
 
+    def select_all(self):
+        conn = sqlite3.connect(f'{self.db_name}.sqlite')
+        cur = conn.cursor()
+        cur.execute(f'SELECT * FROM {self.table_name}')
+        for row in cur:
+            print(row)
+        conn.commit()
+        conn.close()
+
+    def select(self, *args):
+        arguments = ''
+        for arg in args:
+            arguments = arguments + arg + ','
+        conn = sqlite3.connect(f'{self.db_name}.sqlite')
+        cur = conn.cursor()
+        cur.execute(f'SELECT {arguments[:-1]} FROM {self.table_name}')
+        for row in cur:
+            print(row)
+        conn.commit()
+        conn.close()
 
 # db = DataBase('Warszawa')
-tb = Table('Warszawa', 'Pracownicy')
+tb = Table('Warszawa', 'Pracownicy', 'ID','NAME','LAST_NAME','TARGET','SPRZEDAZ','REALIZACJA','KARTY','WKL')
 # tb.create_table('ID','NAME','LAST_NAME','TARGET','SPRZEDAZ','REALIZACJA','KARTY','WKL')
-tb.add_to_table(1,'Sebastian','Waleka',665000,100000,(100000/665000)*100,2,1000)
-# tb.add_to_table(1,'Wojciech','Zielinski',500000,200000,(200000/500000)*100,1,500)
+# tb.add_to_table('Sebastian','Waleka',665000,100000,(100000/665000)*100,2,1000)
+# tb.add_to_table('Wojciech','Zielak',600000,2000000,(200000/600000)*100,5,5500)
+tb.select('ID','NAME','LAST_NAME','KARTY')
