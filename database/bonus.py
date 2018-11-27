40420548ec99e6fdeda6e1080bd55cb8f3363913
sebastianwaleka <sebastian.waleka@gmail.com>
2018-11-27 15:44:50 +0100

    Created new class to create Tables. Its not working properly - must be fixed, especialy add_to_table func

--- database/bonus.py
+++ database/bonus.py
@@ -2,32 +2,51 @@ import sqlite3
 
 
 class DataBase():
-    def __init__(self, db_name, *db_args):
+    def __init__(self, db_name):
         self.db_name = db_name  # Name of Data Base
-        self.db_args = db_args  # Columns of just named Data Base
         try:
-            conn = sqlite3.connect(f'{db_name}.sqlite')
-        except:
-            with open(f'{db_name}.sqlite', w) as db:
+            with open(f'{db_name}.sqlite') as db:
                 db.read()
-            conn = sqlite3.connect(f'{db_name}.sqlite')
+            print(f'Data base named {db_name} already exists.')
+            # conn = sqlite3.connect(f'{db_name}.sqlite')
+        except:
+            with open(f'{db_name}.sqlite', 'w') as db:
+                db.read()
+            print(f'Created data base named {db_name}.')
+            # conn = sqlite3.connect(f'{db_name}.sqlite')
 
-        cur = conn.cursor()
-        cur.execute(f'DROP TABLE IF EXISTS {db_name}')
-        cur.execute(f'CREATE TABLE {db_name} {db_args}')
-        conn.commit()
-        conn.close()
 
-    def add_to_database(self, *args):
+class Table():
+    table_columns = ()
+    def __init__(self, db_name, table_name):
+        self.db_name = db_name
+        self.table_name = table_name
+
+    def create_table(self, *args):
+        self.table_columns = self.table_columns + args
         conn = sqlite3.connect(f'{self.db_name}.sqlite')
         cur = conn.cursor()
-        cur.execute(f'INSERT INTO {self.db_name} {self.db_args} VALUES {args}')
+        cur.execute(f'DROP TABLE IF EXISTS {self.table_name}')
+        cur.execute(f'CREATE TABLE {self.table_name} {args}')
         conn.commit()
         conn.close()
+
+    def add_to_table(self, *tab_args):
+        if len(tab_args) == len(self.table_columns):
+            conn = sqlite3.connect(f'{self.db_name}.sqlite')
+            cur = conn.cursor()
+            cur.execute(f'INSERT INTO {self.table_name} {self.table_columns} VALUES {tab_args}')
+            conn.commit()
+            conn.close()
+        else:
+            print('Number of arguments must be equal to number of columns in table')
+            print(f'{len(tab_args)} - {len(self.table_columns)}')
 
     # def select_branch(self):
 
 
-db = DataBase('employees', 'name', 'last_name')
-
-db.add_to_database('Sebastian', 'Waleka')
+# db = DataBase('Warszawa')
+tb = Table('Warszawa', 'Pracownicy')
+# tb.create_table('ID','NAME','LAST_NAME','TARGET','SPRZEDAZ','REALIZACJA','KARTY','WKL')
+tb.add_to_table(1,'Sebastian','Waleka',665000,100000,(100000/665000)*100,2,1000)
+# tb.add_to_table(1,'Wojciech','Zielinski',500000,200000,(200000/500000)*100,1,500)
--- database/test_bonus.py
+++ database/test_bonus.py
@@ -1,6 +1,6 @@
 import bonus
 
 def test_bonus_branch_create():
-    war = bonus.Branch('Warszawa',2850000)
-    assert war.branch_name == 'Warszawa'
-    assert war.target == 2850000
+    db = bonus.DataBase('Oddzialy', 'Id','Nazwa', 'Target', 'Realizacja', 'Wykonanie', 'Pozostalo')
+    assert db.db_name == 'Oddzialy'
+    assert db.db_args == ('Id','Nazwa', 'Target', 'Realizacja', 'Wykonanie', 'Pozostalo')
